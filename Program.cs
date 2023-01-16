6e519e7e610751a9cfc3d428d757eddd1a94c8a9
Carson Gray <carsongray01@gmail.com>
2023-01-16 10:13:49 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// grid of rooms
+// can't see, can only hear and smell
+// need to find the fountain, restore it, then get to exit
+// based on hunt the wumpus
